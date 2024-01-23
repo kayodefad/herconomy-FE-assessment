a772b2f98e779c416342dd6c915e08bc559a79e8
kayodefad <fadojuk@gmail.com>
2024-01-24 00:34:18 +0100

    fixes: cleanup

--- src/components/RevenueLineChart/index.tsx
+++ src/components/RevenueLineChart/index.tsx
@@ -45,100 +45,11 @@ const LineChartTooltip = ({
 };
 
 const RevenueLineChart = () => {
-  const user = useAppSelector((state) => state.user);
+  const userSlice = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState('');
   const [amount, setAmount] = useState<number | undefined>(undefined);
-
-  const transactions = [
-    {
-      amount: 500,
-      metadata: {
-        name: 'John Doe',
-        type: 'digital_product',
-        email: 'johndoe@example.com',
-        quantity: 1,
-        country: 'Nigeria',
-        product_name: 'Rich Dad Poor Dad',
-      },
-      payment_reference: 'c3f7123f-186f-4a45-b911-76736e9c5937',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-03',
-    },
-    {
-      amount: 400,
-      metadata: {
-        name: 'Fibi Brown',
-        type: 'coffee',
-        email: 'fibibrown@example.com',
-        quantity: 8,
-        country: 'Ireland',
-      },
-      payment_reference: 'd28db158-0fc0-40cd-826a-4243923444f7',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-02',
-    },
-    {
-      amount: 350.56,
-      metadata: {
-        name: 'Delvan Ludacris',
-        type: 'webinar',
-        email: 'johndoe@example.com',
-        quantity: 1,
-        country: 'Kenya',
-        product_name: 'How to build an online brand',
-      },
-      payment_reference: '73f45bc0-8f41-4dfb-9cae-377a32b71d1e',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-01',
-    },
-    {
-      amount: 300,
-      status: 'successful',
-      type: 'withdrawal',
-      date: '2022-03-01',
-    },
-    {
-      amount: 300,
-      metadata: {
-        name: 'Shawn kane',
-        type: 'webinar',
-        email: 'shawnkane@example.com',
-        quantity: 1,
-        country: 'United Kingdom',
-        product_name: 'Support my outreach',
-      },
-      payment_reference: 'c22055e5-8f47-4059-a1e9-51124d325992',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-02-28',
-    },
-    {
-      amount: 200,
-      status: 'successful',
-      type: 'withdrawal',
-      date: '2022-03-01',
-    },
-    {
-      amount: 200,
-      metadata: {
-        name: 'Ada Eze',
-        type: 'webinar',
-        email: 'adaeze1@example.com',
-        quantity: 1,
-        country: 'Nigeria',
-        product_name: 'Learn how to pitch your idea',
-      },
-      payment_reference: '5b2988d9-395e-4a91-984b-8b02f0d12df9',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-02-20',
-    },
-  ];
 
   const handleTransfer = () => {
     dispatch(transfer({ id: +value, amount: amount as number }));
@@ -156,12 +67,12 @@ const RevenueLineChart = () => {
         <div className="flex flex-col gap-[10px]">
           <p className="text-sm font-medium text-[#56616B]">Available Balance</p>
           <p className="text-4xl font-bold">
-            {user.loading ? (
+            {userSlice.loading ? (
               '--'
             ) : (
               <>
                 <span>₦&nbsp;</span>
-                <span>{formatAmount(user.user?.accountBalance || 0)}</span>
+                <span>{formatAmount(userSlice.user?.accountBalance || 0)}</span>
               </>
             )}
           </p>
@@ -192,9 +103,9 @@ const RevenueLineChart = () => {
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Users</SelectLabel>
-                  {user?.users?.map((item) => {
+                  {userSlice?.users?.map((item) => {
                     return (
-                      <SelectItem disabled={item.id === user.user!.id} key={uuid()} value={item.id.toString()}>
+                      <SelectItem disabled={item.id === userSlice.user!.id} key={uuid()} value={item.id.toString()}>
                         {`${item.first_name} ${item.last_name}`}
                       </SelectItem>
                     );
@@ -230,7 +141,7 @@ const RevenueLineChart = () => {
       </div>
       <div className="flex-1">
         <ResponsiveContainer aspect={3.3 / 1}>
-          <LineChart data={transactions} margin={{ top: 0, right: 10, left: 10, bottom: -10 }}>
+          <LineChart data={userSlice.user?.transactions} margin={{ top: 0, right: 10, left: 10, bottom: -10 }}>
             <CartesianGrid vertical={false} horizontal={false} strokeDasharray="3 3" />
             <XAxis
               dataKey="date"
--- src/components/TransactionsList/index.tsx
+++ src/components/TransactionsList/index.tsx
@@ -2,129 +2,17 @@
 
 import React from 'react';
 import { v4 as uuid } from 'uuid';
-import { capitalize, formatAmount } from '@/lib/utils';
+import { formatAmount } from '@/lib/utils';
 import dayjs from 'dayjs';
+import { useAppSelector } from '@/redux/hooks';
 import ArrowIcon from '../IconSVGs/ArrowIcon';
 
-const renderStatus = (status: string) => {
-  let result;
-  switch (status) {
-    case 'successful':
-      result = <span className="text-[#0EA163]">{capitalize(status)}</span>;
-      break;
-    case 'pending':
-      result = <span className="text-[#A77A07]">{capitalize(status)}</span>;
-      break;
-    case 'failed':
-      result = <span className="text-[red]">{capitalize(status)}</span>;
-      break;
-    default:
-      result = <span>{capitalize(status)}</span>;
-  }
-  return result;
-};
-
-const displaySubText = (transaction: any) => {
-  return transaction?.metadata?.name
-    ? transaction?.metadata?.name
-    : transaction?.status
-      ? renderStatus(transaction?.status)
-      : '--';
-};
-
 const TransactionsList = () => {
-  const transactions = [
-    {
-      amount: 500,
-      metadata: {
-        name: 'John Doe',
-        type: 'digital_product',
-        email: 'johndoe@example.com',
-        quantity: 1,
-        country: 'Nigeria',
-        product_name: 'Rich Dad Poor Dad',
-      },
-      payment_reference: 'c3f7123f-186f-4a45-b911-76736e9c5937',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-03',
-    },
-    {
-      amount: 400,
-      metadata: {
-        name: 'Fibi Brown',
-        type: 'coffee',
-        email: 'fibibrown@example.com',
-        quantity: 8,
-        country: 'Ireland',
-      },
-      payment_reference: 'd28db158-0fc0-40cd-826a-4243923444f7',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-02',
-    },
-    {
-      amount: 350.56,
-      metadata: {
-        name: 'Delvan Ludacris',
-        type: 'webinar',
-        email: 'johndoe@example.com',
-        quantity: 1,
-        country: 'Kenya',
-        product_name: 'How to build an online brand',
-      },
-      payment_reference: '73f45bc0-8f41-4dfb-9cae-377a32b71d1e',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-03-01',
-    },
-    {
-      amount: 300,
-      status: 'successful',
-      type: 'withdrawal',
-      date: '2022-03-01',
-    },
-    {
-      amount: 300,
-      metadata: {
-        name: 'Shawn kane',
-        type: 'webinar',
-        email: 'shawnkane@example.com',
-        quantity: 1,
-        country: 'United Kingdom',
-        product_name: 'Support my outreach',
-      },
-      payment_reference: 'c22055e5-8f47-4059-a1e9-51124d325992',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-02-28',
-    },
-    {
-      amount: 200,
-      status: 'successful',
-      type: 'withdrawal',
-      date: '2022-03-01',
-    },
-    {
-      amount: 200,
-      metadata: {
-        name: 'Ada Eze',
-        type: 'webinar',
-        email: 'adaeze1@example.com',
-        quantity: 1,
-        country: 'Nigeria',
-        product_name: 'Learn how to pitch your idea',
-      },
-      payment_reference: '5b2988d9-395e-4a91-984b-8b02f0d12df9',
-      status: 'successful',
-      type: 'deposit',
-      date: '2022-02-20',
-    },
-  ];
+  const userSlice = useAppSelector((state) => state.user);
 
   return (
     <div className="flex flex-col gap-6">
-      {transactions.map((transaction) => {
+      {userSlice?.user?.transactions.map((transaction) => {
         return (
           <div key={uuid()} className="flex items-center justify-between">
             <div className="flex gap-3">
@@ -144,8 +32,8 @@ const TransactionsList = () => {
                 )}
               </div>
               <div className="flex flex-col justify-between font-medium">
-                <p className="text-[#131316]">{transaction?.metadata?.product_name || '--'}</p>
-                <p className="text-sm text-[#56616B]">{displaySubText(transaction)}</p>
+                <p className="text-[#131316]">{transaction?.payment_reference || '--'}</p>
+                <p className="text-sm text-[#56616B]">{`${transaction.type === 'withdrawal' ? 'Withdrawal' : 'Deposit'}`}</p>
               </div>
             </div>
             <div>
